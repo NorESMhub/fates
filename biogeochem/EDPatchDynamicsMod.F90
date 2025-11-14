30159b721578d400f3e93434681050bfc75a9621
Jessica Needham <jessica@login-2.betzy>
2025-11-14 14:46:55 +0100

    silence nocomp patch fusion warning

--- biogeochem/EDPatchDynamicsMod.F90
+++ biogeochem/EDPatchDynamicsMod.F90
@@ -3368,11 +3368,11 @@ contains
                 endif
              end do
 
-             if ( .not. gotfused ) then
-                !! somehow didn't find a patch to fuse with.
-                write(fates_log(),*) 'Warning. small nocomp patch wasnt able to find another patch to fuse with.', &
-                     currentPatch%nocomp_pft_label, currentPatch%land_use_label, currentPatch%area
-             endif
+             ! if ( .not. gotfused ) then
+             !    !! somehow didn't find a patch to fuse with.
+             !    write(fates_log(),*) 'Warning. small nocomp patch wasnt able to find another patch to fuse with.', &
+             !         currentPatch%nocomp_pft_label, currentPatch%land_use_label, currentPatch%area
+             ! endif
 
           else nocomp_if
 
